f11aab578b667ea07426a4ce5f325c70c41be1ea
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-03 08:30:17 +0000

    changed print format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,7 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if j < 9 else "\n")
+        if (i == 8 and j == 9):
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
