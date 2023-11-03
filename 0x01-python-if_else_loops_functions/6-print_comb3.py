d8852b036e3a357b829a7357d3f7f695277c5e17
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-03 08:27:27 +0000

    update logic

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        if (i==8 and j == 9):
-            print(f"{i}{j}")
-        else:
-            print(f"{i}{j}", end=", ")
+        print(f"{i}{j}", end=", " if j < 9 else "\n")
