9dfc3403920bfdca23122b9bca7824406ef0be37
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-03 07:24:54 +0000

    improved logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
+
+# using modolus to get the last  digit of number
+last_digit = abs(number) % 10
+# apply sign
+if number < 0:
+    last_digit *= -1
 
 if last_digit > 5:
     print(
             f"Last digit of {number} is "
             f"{last_digit} and is greater than 5"
         )
-
-if last_digit == 0:
+elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-
-if last_digit < 6 and last_digit != 0:
+else:
     print(
             f"Last digit of {number} is "
             f"{last_digit} and is less than 6 and not 0"
