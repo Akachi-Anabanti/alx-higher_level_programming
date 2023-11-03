734f56d123fc06bb55d3f244461cabd8eeb2a45f
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-03 08:04:24 +0000

    print numbers in 2 digits

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# print number in ascending order from 0 to 99
+
+for number in range(100):
+    if number == 99:
+        print(number)
+    else:
+        print("{:02d}".format(number), end=",")
