63327a8ceac704502d60031d0e72abf58ae875a4
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-03 15:16:06 +0000

    copy and replace char in string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    str_cp = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        str_cp += str[i]
+    return str_cp
