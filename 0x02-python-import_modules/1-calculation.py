f0e7e0c39e35ba60a6b753204416e0fcc880f872
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-04 23:25:45 +0000

    import from file and excute funcs

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# import modules
+import calculator_1 as calculator
+
+
+def calculate():
+    """Perform calculations"""
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calculator.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator.div(a, b)))
+
+
+if __name__ == "__main__":
+    calculate()
