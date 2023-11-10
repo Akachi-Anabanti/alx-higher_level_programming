44c13e15e42e43444a34de61be4610a6bd9466f8
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-10 15:46:53 +0000

    logic improvement

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,8 @@ def weight_average(my_list=[]):
     tup = ((A1,B1), (A2, B2))
     weighted_average = sum(Ai x Bi) / sum(Bi) from i = 0 to i = i
     """
-    total_prod = sum([x * y for x, y in my_list])
-    total_weight = sum([item[1] for item in my_list])
-    return total_prod / total_weight
+    if isinstance(my_list, list) and len(my_list) > 0:
+        total_prod = sum([x * y for x, y in my_list])
+        total_weight = sum([item[1] for item in my_list])
+        return total_prod / total_weight
+    return 0
