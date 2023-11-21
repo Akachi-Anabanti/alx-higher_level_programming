8458336266a0b7fa9b998336f23c4d4e69ada820
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-21 20:28:33 +0000

    print err to stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as ve:
+        print("Exception:", ve, file=sys.stderr)
+        return False
+    except TypeError as te:
+        print("Exception:", te, file=sys.stderr)
+        return False
+    else:
+        return True
