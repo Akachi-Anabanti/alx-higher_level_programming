3a2814a25484f2c588c8b313a32af907438be729
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-23 18:13:43 +0000

    a class that instantiates a prive attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A module that creates a square class"""
+
+
+class Square:
+    """A class that instantiates a private size attribute
+
+    Attributes:
+        __size: The size of the square"""
+
+    def __init__(self, size):
+        """The initialization of the class
+            Args:
+                si
+        """
+        self.__size = size
