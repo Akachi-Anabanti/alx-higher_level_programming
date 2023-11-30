b7d09a345a422353a150c83649b8b5b2192725c0
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-11-30 04:46:23 +0000

    empty rectangle class

---
+++ 0x08-python_more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: Anabanti Akachi
+# --*-- coding: utf-8 --*--
+"""This module creates an empty class"""
+
+
+class Rectangle:
+    """An Empty Rectangle class"""
+    pass
