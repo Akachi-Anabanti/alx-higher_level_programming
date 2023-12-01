f70782b838effa2bd374b79e8d9a581faa069d55
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-01 08:48:45 +0000

    repr method on class

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+# Author: Anabanti Akachi
+
+"""This module creates a rectangle class"""
+
+
+class Rectangle:
+    """A Rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width attr"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height attr"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """prints the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(['#' * self.__width for _ in range(self.__height)])
+
+    def __repr__(self):
+        """Prints a class instance of the class"""
+        return "Rectangle(" + \
+                str(self.__width) + \
+                "," + \
+                str(self.__height) + ")"
