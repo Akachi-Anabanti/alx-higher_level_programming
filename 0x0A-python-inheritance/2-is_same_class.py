539d54154d3f85a997fe2d33872b81a0b06cf42f
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-10 19:35:36 +0000

    used type obj

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,6 +9,6 @@ an object is an instance of a specified class
 def is_same_class(obj, a_class):
     """Returns True if obj is instance of a_class"""
 
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return (True)
     return (False)
