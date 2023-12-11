7190e88728a9ecd6268799ae425337d3162307d9
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-11 13:01:38 +0000

    refactor removed area method

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -19,9 +19,4 @@ class Square(Rectangle):
         """
 
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
-
-    def area(self):
-        """Returns the area of the square"""
-        return self.__size * self.__size
