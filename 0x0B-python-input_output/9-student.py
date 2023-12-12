2bb847335e4dbb5c5331031807eae1a03d9e7a7e
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-12 15:22:25 +0000

    student class that returns json instance

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Author: Anabanti Akachi
+
+"""A Class that defines a student"""
+
+
+class Student:
+    """A class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of
+        the class"""
+
+        return (vars(self))
