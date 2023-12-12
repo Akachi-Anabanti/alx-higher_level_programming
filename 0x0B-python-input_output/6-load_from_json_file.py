edb00b46acd5483f225f9a327a8c3772fdd1d312
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-12 14:18:41 +0000

    refactor

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Author: Anabanti Akachi
 
-"""Defines a function that creates an object
+"""Defines a function that creates an obj
 supplied by a JSON file"""
 import json
 
@@ -10,4 +10,5 @@ def load_from_json_file(filename):
     """loads object from JSON file"""
 
     with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+        data = json.load(f)
+    return data
