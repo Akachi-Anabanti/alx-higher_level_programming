5cb0f7cae2e49c97f953138b4e659684fc5a76f0
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-12 13:13:08 +0000

    read a text file at once

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author: Anabanti Akachi
+
+"""Supplies a function that reads a text file"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints to stdout"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read().strip("\n"))
