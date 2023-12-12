ed63e456c66a47db37762c9ef93e4379d96e1b9f
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-12 16:05:40 +0000

    improved doc

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,16 +9,25 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the class"""
+        """Initializes the class
+        Args:
+            first_name (str): The firstname
+            last_name (str): The last name
+            age (int): The age
+        """
         self.age = age
         self.last_name = last_name
         self.first_name = first_name
 
     def to_json(self, attrs=None):
         """Returns a dictionary representation of
-        the class for the attr"""
+        the class for the attr
+        Args:
+            attrs (list, optional): A list of str of
+            required attrs
+        """
 
-        if attrs and type(attrs) == list \
+        if type(attrs) == list \
                 and all(type(item) == str for item in attrs):
             return {name: getattr(self, name) for name in attrs
                     if hasattr(self, name)}
