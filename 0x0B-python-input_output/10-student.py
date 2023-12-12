cd9f75ada8b62155c4f11c4afc6f5b780d9a4bff
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-12 15:56:08 +0000

    changed order of attr iinitialization

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,10 +10,9 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initializes the class"""
-
-        self.first_name = first_name
-        self.last_name = last_name
         self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
 
     def to_json(self, attrs=None):
         """Returns a dictionary representation of
