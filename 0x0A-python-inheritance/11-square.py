902be2efb040801ea30c17cd11ade3e38340b60e
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-12 09:28:34 +0000

    refactor

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-# Author: Anabanti Akachi
 
-"""Defines a square class that inherits
-from Rectangle class"""
+# Author: Anabanti Akachi
+"""Defines a square inherits Rectangle """
 
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Represents the square class"""
+    """Square which inherits the Rectangle
+    """
 
     def __init__(self, size):
-        """instantiates the class with size"""
+        """instantiates with size
+        Args:
+            size (int): The size of the square
+        """
 
         self.integer_validator("size", size)
         self.__size = size
 
-        super().__init__(size, size)
+        super().__init__(self.__size, self.__size)
