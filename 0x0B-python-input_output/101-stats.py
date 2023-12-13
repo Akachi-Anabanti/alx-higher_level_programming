00759878d1be65adfe8d0993c79211e6703ccf78
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-13 23:31:23 +0000

    refactored removed func definitions

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # Author: Anabanti Akachi
 
-"""Defines two functions get_stats that reads
-extracts stats in a network packet and print_stats
-that prints the stats in specified format
+"""Defines a script that reads network packet of the form
+`<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>`
+and prints the stats in specified format
+
 `
 File size: 14525
 200: 1
@@ -15,30 +17,23 @@ import sys
 from collections import defaultdict
 
 
-def get_stats():
-    """Extracts the data from the packet"""
-    total_size = 0
-    status_codes = defaultdict(int)
+# Extracts the data from the packet
+total_size = 0
+status_codes = defaultdict(int)
 
-    try:
-        for i, line in enumerate(sys.stdin):
-            parts = line.split(" ")
-            status_code = parts[-2]
-            file_size = int(parts[-1])
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        parts = line.split(" ")
+        status_code = parts[-2]
+        file_size = int(parts[-1])
 
-            total_size += file_size
-            status_codes[status_code] += 1
-            if i % 10 == 0:
-                print_stats(total_size, status_codes)
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-
-
-def print_stats(total_size, status_codes):
-    """prints the stats in correct format"""
+        total_size += file_size
+        status_codes[status_code] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(total_size))
+            for status_code in sorted(status_codes.keys()):
+                print("{}:{}".format(status_code, status_codes[status_code]))
+except KeyboardInterrupt:
     print("File size: {}".format(total_size))
     for status_code in sorted(status_codes.keys()):
         print("{}:{}".format(status_code, status_codes[status_code]))
-
-
-get_stats()
