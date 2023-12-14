725e596d39815efa63434b56e9dd4aef8c64ab04
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-14 17:05:11 +0000

    ouput fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,7 +85,7 @@ class Rectangle(Base):
     def y(self, value):
         """y attr setter"""
         if type(value) != int:
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
