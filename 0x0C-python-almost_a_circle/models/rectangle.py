e0678740fbe11e052632611ee6a4029f313d47d0
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-14 15:48:09 +0000

    [minor] fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,51 +26,51 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """width property getter
-            Returns:
-                The value of width
-            """
-            return self.__width
+    @property
+    def width(self):
+        """width property getter
+        Returns:
+            The value of width
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """width attr setter
-            Arg:
-            """
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """width attr setter
+        Arg:
+        """
+        self.__width = value
 
-        @property
-        def height(self):
-            """height attr getter"""
-            return self.__height
+    @property
+    def height(self):
+        """height attr getter"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """height attr setter
-            Args:
-                height (int):
-            """
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """height attr setter
+        Args:
+            height (int):
+        """
+        self.__height = value
 
-        @property
-        def x(self):
-            """x attr getter"""
-            return self.__x
+    @property
+    def x(self):
+        """x attr getter"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """x attr setter"""
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """x attr setter"""
+        self.__x = value
 
-        @property
-        def y(self):
-            """y attrr getter"""
-            return self.__y
+    @property
+    def y(self):
+        """y attrr getter"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """y attr setter"""
+    @y.setter
+    def y(self, value):
+        """y attr setter"""
 
-            self.__y = value
+        self.__y = value
