8b27581cfd7545152beaa98be09981ac9f04193a
Akachi Anabanti <akachi.anabanti@gmail.com>
2023-12-15 12:34:00 +0000

    to json representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 # Author: Anabanti Akachi
 
 """Defines a Base class"""
+import json
 
 
 class Base:
@@ -20,3 +21,11 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        """returns the json representation of
+        list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '"[]"'
+        return json.dumps(list_dictionaries)
