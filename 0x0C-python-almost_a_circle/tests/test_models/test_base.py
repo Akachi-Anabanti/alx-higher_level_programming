67bef7b51a07c46d6c6d5065b9ceddd60bca2021
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-02 17:27:09 +0000

    improved base test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,5 +8,9 @@ class BaseTest(unittest.TestCase):
 
     def test_public_instance(self):
         base = Base()
-        assert(base.id == 1)
+        base_0 = Base();
+        base_2 = Base(3);
+        self.assertEqual(base.id, 1)
+        self.assertEqual(base_0.id, 2)
+        self.assertEqual(base_2.id, 3)
 
