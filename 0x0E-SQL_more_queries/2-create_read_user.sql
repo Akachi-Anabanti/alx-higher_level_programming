42416cedfa4d2c9da010252f5667045d311f8ac1
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-09 21:11:10 +0000

    create db, user and set privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- create database if not exists
+-- create user and grant SELECT privilege only
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
