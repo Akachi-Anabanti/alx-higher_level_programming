56090dc7ebf4b26df4c764ebad48924615840f60
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-09 21:57:29 +0000

    requirement fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,7 @@
 -- lists all cities of california that can be found in
 -- in the db
-USE hbtn_0d_usa;
 SELECT * FROM cities
 WHERE state_id=(SELECT id from states
-       WHERE name='California');
+       WHERE name='California')
+ORDER BY id ASC
+;
