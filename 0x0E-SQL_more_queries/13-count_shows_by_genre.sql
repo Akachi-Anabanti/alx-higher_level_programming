12f60d56521ee99d597640865edfdec641428dc3
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-10 23:12:40 +0000

    letter case fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- number of shows by genre
 
-SELECT name AS genre, COUNT(*) as number_of_shows FROM tv_genres
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
