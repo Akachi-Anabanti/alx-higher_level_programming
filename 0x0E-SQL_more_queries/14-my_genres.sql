5a446ca6b38799ff6ca17d8e588ac4d26774c5f6
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-10 23:34:38 +0000

    sorting fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,5 @@
 SELECT name FROM tv_shows 
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_genres.id = genre_id 
-WHERE title='Dexter';
+WHERE title='Dexter'
+ORDER BY name ASC;
