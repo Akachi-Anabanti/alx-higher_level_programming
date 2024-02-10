297c7eef67ffdb44027b1964b8cc97e1815a3055
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-10 23:32:41 +0000

    my genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- list genre by title
+
+SELECT name FROM tv_shows 
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = genre_id 
+WHERE title='Dexter';
