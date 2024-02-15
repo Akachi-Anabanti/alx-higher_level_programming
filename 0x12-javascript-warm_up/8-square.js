dbcb12e19fd3e5d2b46fa552ff2507f03885f229
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-15 12:32:05 +0000

    case changed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const { argv } = require('node:process');
 if (argv[2] && parseInt(argv[2])) {
   const size = parseInt(argv[2]);
   for (let i = 0; i < size; i++) {
-    const repeated = 'x'.repeat(size);
+    const repeated = 'X'.repeat(size);
     console.log(repeated);
   }
 } else {
