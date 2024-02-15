e34c2615d669bf1ed4634b44073a91f115122fd3
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-15 12:30:27 +0000

    print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv[2] && parseInt(argv[2])) {
+  const size = parseInt(argv[2]);
+  for (let i = 0; i < size; i++) {
+    const repeated = 'x'.repeat(size);
+    console.log(repeated);
+  }
+} else {
+  console.log('Missing size');
+}
