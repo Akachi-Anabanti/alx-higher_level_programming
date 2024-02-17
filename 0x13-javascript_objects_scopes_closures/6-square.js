0874d30037b4a609eb079892e22d5317b1797649
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-17 13:15:09 +0000

    req [-fix]

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const BaseSquare = require('./5-square');
+module.exports = class Square extends BaseSquare {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       const wdth = c.repeat(this.width);
