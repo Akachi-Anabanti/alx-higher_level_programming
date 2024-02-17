75ef300a9c316c3caf04014a7fc7f76a6f11b2c2
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-17 10:02:41 +0000

    more methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const wdth = 'X'.repeat(this.width);
+      console.log(wdth);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
