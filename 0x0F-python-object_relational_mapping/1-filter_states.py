49bb6d3afa53e79525998abee8a490790c8550d8
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-20 10:19:09 +0000

    binary keyword fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ def list_states(username, password, database):
             )
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC;")
 
     [print(item) for item in cur.fetchall()]
 
