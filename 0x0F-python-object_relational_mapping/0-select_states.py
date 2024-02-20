8e0953eb467321b8a4b2baf468f272573ca6b367
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-20 10:01:51 +0000

    list all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""A script that lists all states from the
+"""
+import MySQLdb
+
+
+def list_states(username, password, database):
+    """Executes the database command"""
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    [print(item) for item in cur.fetchall()]
+
+
+if __name__ == "__main__":
+    import sys
+    list_states(sys.argv[1], sys.argv[2], sys.argv[3])
