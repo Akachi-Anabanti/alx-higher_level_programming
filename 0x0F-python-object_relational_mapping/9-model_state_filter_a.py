2eaadf47186186a36bc540e641dd4e03508bd772
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-02-23 18:40:55 +0000

    Filter query

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, asc
+
+
+def list_states(name, password, db):
+    """Lists all states"""
+    # create the engine
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                name,
+                password, db),
+            pool_pre_ping=True
+            )
+    # Create session
+    Session = sessionmaker(bind=engine)
+
+    # start a new session
+    session = Session()
+
+    states = session.query(State).filter(State.name.contains('a'))
+    .order_by(asc(State.id))
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    list_states(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3])
