44621a85d7ccac9f4054000c6568f54ad61189cb
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-09 15:57:08 +0000

    binary search time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,10 +6,16 @@ peak of a list of integers
 
 
 def find_peak(list_of_integers):
-    """A function that finds the peak
-    of a list of integers
-    """
-    if type(list_of_integers) != list or\
-       len(list_of_integers) == 0:
+    """A function that finds the peak of a list
+    of integers"""
+    if not list_of_integers:
         return None
-    return max(list_of_integers)
+    low = 0
+    high = len(list_of_integers) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+    return list_of_integers[low]
