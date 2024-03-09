049d4bb0e38e30089f9cfa727381660e18131938
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-09 14:58:35 +0000

    fix line count

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# A scripts that takes a url and displays the 
-#+ size of the body of the response
-
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <url>"
-  exit 1
-fi
-
+# A scripts that takes a url and displays the
 curl -s -o ./file.tmp -w "%{size_download}\n" "$1"
