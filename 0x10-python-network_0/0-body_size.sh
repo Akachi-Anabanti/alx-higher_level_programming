c6c479d081643b01f34e1d5ef73839a337f97831
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-09 14:55:52 +0000

    size of response body

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# A scripts that takes a url and displays the 
+#+ size of the body of the response
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+curl -s -o ./file.tmp -w "%{size_download}\n" "$1"
