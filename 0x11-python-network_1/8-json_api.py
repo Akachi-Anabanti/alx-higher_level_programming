662b3b780b0f4b716d8c91be54fe73f1db654fd7
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-10 22:26:25 +0000

    error and var fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         letter = ""
     else:
-        letter = sys.argv[0]
+        letter = sys.argv[1]
 
     re = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
 
     try:
         data = re.json()
         if data == {}:
-            print("JSON is empty")
+            print("No result")
         else:
             print("[{}] {}".format(data.get("id"), data.get("name")))
-    except requests.JSONDecodeError:
+    except ValueError:
         print("Not a valid JSON")
