6a95c258e9184d2e7e3d3291d18e95c546575223
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-10 22:46:53 +0000

    fix, typo

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    re = request.post(url, data={"email": email})
+    re = requests.post(url, data={"email": email})
 
     print(re.text)
