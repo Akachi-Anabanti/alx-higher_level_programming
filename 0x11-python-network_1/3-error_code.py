7fa088510d45a523125937561bce9ce8bb7f06ca
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-10 20:59:23 +0000

    Http error code handling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that takes a url, sends a request
+to the url and displays the body of the
+response"""
+import urllib.request
+import sys
+
+
+if __name == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
