74a2ff43342303e341752ff6d9f80b32c5fe0d94
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-10 20:30:02 +0000

    output format fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         page = response.read()
         print("Body response:")
-        print("\t - type: {}".format(type(page)))
-        print("\t - content: {}".format(page))
-        print("\t - utf8 content: {}".format(page.decode("utf-8")))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode()))
