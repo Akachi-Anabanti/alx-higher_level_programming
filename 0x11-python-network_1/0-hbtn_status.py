2861b5b399f2c4bffe193ce810940dad5b45cae0
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-10 20:22:55 +0000

    indentation change

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
             )
     with urllib.request.urlopen(req) as response:
         page = response.read()
-
-    print("Body response:")
-    print("\t - type: {}".format(type(page)))
-    print("\t - content: {}".format(page))
-    print("\t - utf8 content: {}".format(page.decode(encoding="utf-8")))
+        print("Body response:")
+        print("\t - type: {}".format(type(page)))
+        print("\t - content: {}".format(page))
+        print("\t - utf8 content: {}".format(page.decode("utf-8")))
